a46deae84ee1c82790782b28c534b4e5e69c8f86
Karan Mahajan <karanmahajan321@gmail.com>
2026-05-04 12:01:50 -0400

    Add Google Tag Manager and Google Analytics scripts

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -56,6 +56,18 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <Script id="gtm-head" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MC74PWVQ');`}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JP4C1GS4BT"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JP4C1GS4BT');
+          `}
+        </Script>
       </head>
       <body>
         <a href="#main-content" className="km-skip-link">Skip to main content</a>
